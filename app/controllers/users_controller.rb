5644351e3a5fbff50f1e7504da4f84a568ed23b4
Charlotte Weager <senwyn@googlemail.com>
2021-05-29 16:40:17 +0100

    Added buttons scss

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @stamp_count = @user.stamps.where(stamp_status: true).count
+    @stamps = @user.stamps
+    @stamp_count = @stamps.where(stamp_status: true).count
+    @markers = @stamps.locations.geocoded.map do |location|
+      {
+        lat: location.latitude,
+        lng: location.longitude
+      }
+    end
   end
 end
