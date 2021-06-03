a53019d10bd25ec4c37d3fa763e2819aaef042f9
Charlotte Weager <senwyn@googlemail.com>
2021-06-03 20:37:37 +0100

    Updated the stampbook bugs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,7 +29,7 @@ import { initMapbox } from '../plugins/init_mapbox';
 import { toggleMenu } from '../plugins/init_menu';
 import { hideProfile } from '../plugins/init_hide_profile';
 import { addSelectedClass } from '../plugins/add_selected';
-import { pageFlip } from './turning-pages.js';
+import { flipPages } from './turning-pages.js';
 import 'controllers'
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
@@ -38,7 +38,9 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // addSelectedClass();
   // initSelect2();
-  pageFlip();
+  if(document.querySelectorAll('.my-page')){
+    flipPages();
+  };
   initMapbox();
   toggleMenu();
   hideProfile();
--- app/javascript/plugins/turning-pages.js
+++ app/javascript/plugins/turning-pages.js
@@ -1,13 +1,13 @@
 import { PageFlip } from 'page-flip';
+const flipPages = () =>{
+	const pageFlip = new PageFlip(document.getElementById('book'), {
+			width: 600, // required parameter - base page width
+			height: 400, // required parameter - base page height
+			maxHeight: 400,
+			drawShadow: true,
+			maxShadowOpacity: 0.3
+	});
 
-const pageFlip = new PageFlip(document.getElementById('book'), {
-    width: 600, // required parameter - base page width
-    height: 400, // required parameter - base page height
-    maxHeight: 400,
-    drawShadow: true,
-    maxShadowOpacity: 0.3
-});
-
-pageFlip.loadFromHTML(document.querySelectorAll('.my-page'));
-
-export { pageFlip };
+	pageFlip.loadFromHTML(document.querySelectorAll('.my-page'));
+}
+export { flipPages };
