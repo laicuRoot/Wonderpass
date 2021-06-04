985450ab308479698545bd123821abf887d1bf71
Jose Blanco <jdblancoes@gmail.com>
2021-06-04 21:46:20 +0100

    Working btns to set itineraries active

--- app/javascript/plugins/add_active_itinerary.js
+++ app/javascript/plugins/add_active_itinerary.js
@@ -1,18 +1,21 @@
-const addActiveItinerary = () => {
-  const itineraries = document.querySelectorAll('#itinerary-btn');
-  if(itineraries){
-    itineraries.forEach((itinerary) => {
-      itinerary.addEventListener('click', (event) => {
-        if(event.currentTarget.classList.contains('active-itinerary')){
-          event.currentTarget.classList.remove('active-itinerary');
-          event.currentTarget.innerText = 'Set Active'
-        } else {
-          event.currentTarget.classList.add('active-itinerary');
-          event.currentTarget.innerText = 'Active'
-        }
-      })
-    })
+const active = (e) => {
+  e.preventDefault();
+  const active = document.querySelector('.active-itinerary');
+  if(active){
+    active.classList.remove('active-itinerary');
+    active.innerText = 'Set Active'
   }
+  event.currentTarget.classList.add('active-itinerary')
+  event.currentTarget.innerText = 'Active'
 }
+
+const addActiveItinerary = () => {
+  const itinerariesBtns = document.querySelectorAll('#itinerary-btn');
+  itinerariesBtns.forEach((itinerary)=>{
+    itinerary.addEventListener('click', active)
+  })
+}
+
+
 
 export {addActiveItinerary}
