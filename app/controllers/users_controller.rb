18da4459c152e6805c106ae0ad09b20de44ea179
Alexander Vassilev <alexander.vassilev.av@gmail.com>
2021-06-12 23:02:06 +0100

    made some changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user_active_itinerary = Itinerary.where(active_itinerary: true).where(user: current_user)
     @stamps = @user_active_itinerary.empty? ? Stamp.all : @user_active_itinerary.map(&:stamps).flatten
     @stamps_all = Stamp.all.where(id: @stamps)
-    @stamp_count = @current_user.stamps.count
+    @stamp_count = @user.stamps.where(stamp_status: true).count
     @locations = Location.where(id: @stamps.map(&:location_id))
     @markers = @locations.geocoded.map do |location|
       {
