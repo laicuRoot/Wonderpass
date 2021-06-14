884b322c3eafe3bf9018dad7796088ea41057f90
Charlotte Weager <senwyn@googlemail.com>
2021-06-14 22:14:49 +0100

    adjusted the indentation within itinerary controller

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -1,26 +1,26 @@
 class ItinerariesController < ApplicationController
   before_action :find_user, except: [:filter, :destroy, :activate]
 
-def index
-  # @stamps_all = Stamp.all.where(user: @stampbooks..user)
-  # @locations = Location.all
-  @itineraries = Itinerary.where(user: @user)
-  @user_active_itinerary = Itinerary.get_active(@user)
-  @stamps = @user_active_itinerary.map(&:stamps).flatten
-  @stamps_all = Stamp.all.where(id: @stamps)
-  @all_int_stamps = @itineraries.map(&:stamps).flatten
-  # @stamps = @itineraries.map{ |itinerary| itinerary.stamps }.flatten
-  @locations = Location.where(id: @stamps.map(&:location_id))
-  @itinerary_markers = @locations.geocoded.map do |location|
-    {
-      lat: location.latitude,
-      lng: location.longitude,
-      stampWindow: render_to_string(partial: "stamp_itinerary_window", locals: { stamp: @stamps_all.find_by(location: location) }),
-      imageUrl: helpers.asset_url("http://res.cloudinary.com/laicuroot/image/upload/c_fill,h_40,w_40/"+ location.stamp_photos.first.key),
-      stampStatus: @stamps_all.find_by(location: location).stamp_status
-    }
+  def index
+    # @stamps_all = Stamp.all.where(user: @stampbooks..user)
+    # @locations = Location.all
+    @itineraries = Itinerary.where(user: @user)
+    @user_active_itinerary = Itinerary.get_active(@user)
+    @stamps = @user_active_itinerary.map(&:stamps).flatten
+    @stamps_all = Stamp.all.where(id: @stamps)
+    @all_int_stamps = @itineraries.map(&:stamps).flatten
+    # @stamps = @itineraries.map{ |itinerary| itinerary.stamps }.flatten
+    @locations = Location.where(id: @stamps.map(&:location_id))
+    @itinerary_markers = @locations.geocoded.map do |location|
+      {
+        lat: location.latitude,
+        lng: location.longitude,
+        stampWindow: render_to_string(partial: "stamp_itinerary_window", locals: { stamp: @stamps_all.find_by(location: location) }),
+        imageUrl: helpers.asset_url("http://res.cloudinary.com/laicuroot/image/upload/c_fill,h_40,w_40/"+ location.stamp_photos.first.key),
+        stampStatus: @stamps_all.find_by(location: location).stamp_status
+      }
+    end
   end
-end
 
   def show
     @itinerary = Itinerary.find(params[:id])
