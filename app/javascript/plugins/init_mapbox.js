ee6c27aac7450631bba616c4001000f2c9e6a332
Alexander Vassilev <alexander.vassilev.av@gmail.com>
2021-06-17 21:20:14 +0100

    hide the toggle button if no active itin

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -45,29 +45,31 @@ const getMapElements = () => {
 
 const toggleMarkers = (map, mapElement) =>{
   const toggle = document.querySelector(".toggle-all");
-  const activeMarkers = JSON.parse(mapElement.dataset.markers);
-  const allMarkers = JSON.parse(mapElement.dataset.allMarkers);
-  toggle.addEventListener("click", event =>{
-    let target = event.currentTarget
-    if (target.innerText == "All Stamps"){
-      target.innerText = "View Active";
-      $('.mapboxgl-marker').remove();
-      addMarkers(map, allMarkers);
-      fitMapToMarkers(map, allMarkers);
-    } else if (target.innerText == "View Active"){
-      target.innerText = "All Stamps";
-      $('.mapboxgl-marker').remove();
-      addMarkers(map, activeMarkers);
-      fitMapToMarkers(map, activeMarkers);
-    }
-  });
+  if (toggle) {
+    const activeMarkers = JSON.parse(mapElement.dataset.markers);
+    const allMarkers = JSON.parse(mapElement.dataset.allMarkers);
+    toggle.addEventListener("click", event =>{
+      let target = event.currentTarget
+      if (target.innerText == "All Stamps"){
+        target.innerText = "View Active";
+        $('.mapboxgl-marker').remove();
+        addMarkers(map, allMarkers);
+        fitMapToMarkers(map, allMarkers);
+      } else if (target.innerText == "View Active"){
+        target.innerText = "All Stamps";
+        $('.mapboxgl-marker').remove();
+        addMarkers(map, activeMarkers);
+        fitMapToMarkers(map, activeMarkers);
+      }
+    });
+  }
 }
 
 const myLocation = (map) => {
   // Add geolocate control to the map.
   map.addControl(new mapboxgl.GeolocateControl(
 	  { positionOptions: { enableHighAccuracy: true },
-	   trackUserLocation: true, 
+	   trackUserLocation: true,
 	   fitBoundsOptions: {maxZoom: 7}
 	  })
 	);
