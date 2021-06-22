82c37113e482c72d0a421f0edcdada237f348a0b
Robert Mafeni <robertmafeni@gmail.com>
2021-06-22 20:52:05 +0100

    more styles fixes :)

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -4,7 +4,7 @@ class Itinerary < ApplicationRecord
   has_many :stamps, through: :itinerary_items
   # validates :rating, numericality: true
   # validates :rating, inclusion: { in: [1..5] }
-  validates :public_status, inclusion: [true, false] 
+  # validates :public_status, inclusion: [true, false]
   validates :itinerary_name, :destination, presence: true
   # before_save :set_active
 
